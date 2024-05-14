49ffdf830e401f04305b7485a21dc12ff15f9d3e
chanu <cw8881@naver.com>
2024-05-14 23:00:51 +0900

    #10 [delete] 불필요한 코드 제거

--- app/src/main/java/com/sopt/now/gs/feature/mypage/MyPageFragment.kt
+++ app/src/main/java/com/sopt/now/gs/feature/mypage/MyPageFragment.kt
@@ -2,7 +2,6 @@ package com.sopt.now.gs.feature.mypage
 
 import com.sopt.now.gs.R
 import com.sopt.now.gs.core.base.BindingFragment
-import com.sopt.now.gs.databinding.FragmentHomeBinding
 import com.sopt.now.gs.databinding.FragmentMyPageBinding
 
 class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
