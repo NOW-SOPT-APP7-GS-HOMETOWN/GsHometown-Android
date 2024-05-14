2fb16395512bc512652fcd962610a0cbb2d235d5
chanu <cw8881@naver.com>
2024-05-15 02:00:03 +0900

    #10 [add] 더미 사전예약 fragment 추가

---
+++ app/src/main/java/com/sopt/now/gs/feature/reserve/ReserveFragment.kt
@@ -0,0 +1,15 @@
+package com.sopt.now.gs.feature.reserve
+
+import com.sopt.now.gs.R
+import com.sopt.now.gs.core.base.BindingFragment
+import com.sopt.now.gs.databinding.FragmentReserveBinding
+import com.sopt.now.gs.feature.util.KeyStorage
+
+class ReserveFragment : BindingFragment<FragmentReserveBinding>(R.layout.fragment_reserve) {
+    override fun initView() {
+        val memberId = arguments?.getInt(KeyStorage.USER_ID) ?: -1
+        if (memberId != -1) {
+            binding.tvReserveExample.text = "Member ID: $memberId"
+        }
+    }
+}
