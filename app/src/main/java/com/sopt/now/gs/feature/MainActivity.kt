106fbca1c77af7de267c53413c1d9d7df879fa1f
chanu <cw8881@naver.com>
2024-05-14 23:37:44 +0900

    #10 [feat] jetpack navi 구현

--- app/src/main/java/com/sopt/now/gs/feature/MainActivity.kt
+++ app/src/main/java/com/sopt/now/gs/feature/MainActivity.kt
@@ -2,12 +2,26 @@ package com.sopt.now.gs.feature
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.sopt.now.gs.R
+import com.sopt.now.gs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setContentView(R.layout.activity_main)
+        initMainBottomNavigation()
+    }
+
+    private fun initMainBottomNavigation() {
+        val navController =
+            (supportFragmentManager.findFragmentById(R.id.fcv_home) as NavHostFragment)
+                .findNavController()
+        binding.bnvHome.setupWithNavController(navController)
     }
 }
