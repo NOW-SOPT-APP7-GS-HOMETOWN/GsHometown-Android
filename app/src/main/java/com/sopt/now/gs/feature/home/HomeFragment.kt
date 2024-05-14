ce206c018c87e406a08a75ffcb8f069445735ab9
chanu <cw8881@naver.com>
2024-05-15 02:00:57 +0900

    #10 [feat] 홈에서 사전예약 화면 이동

--- app/src/main/java/com/sopt/now/gs/feature/home/HomeFragment.kt
+++ app/src/main/java/com/sopt/now/gs/feature/home/HomeFragment.kt
@@ -1,11 +1,22 @@
 package com.sopt.now.gs.feature.home
 
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
 import com.sopt.now.gs.R
 import com.sopt.now.gs.core.base.BindingFragment
 import com.sopt.now.gs.databinding.FragmentHomeBinding
+import com.sopt.now.gs.feature.util.KeyStorage
 
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override fun initView() {
-//        TODO("Not yet implemented")
+        val id = 1
+        binding.btnHomeReserve.setOnClickListener { navigateToReserve(id) }
+    }
+
+    private fun navigateToReserve(id: Int) {
+        findNavController().navigate(
+            R.id.fragment_reserve,
+            bundleOf(KeyStorage.USER_ID to id),
+        )
     }
 }
