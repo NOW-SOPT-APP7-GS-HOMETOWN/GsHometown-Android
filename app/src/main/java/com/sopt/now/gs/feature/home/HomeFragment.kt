dbc8d3c53a085c19b1981a12c2a4db4af3a438a8
chanu <cw8881@naver.com>
2024-05-14 22:59:16 +0900

    #10 [add] 더미 fragment 추가

---
+++ app/src/main/java/com/sopt/now/gs/feature/home/HomeFragment.kt
@@ -0,0 +1,11 @@
+package com.sopt.now.gs.feature.home
+
+import com.sopt.now.gs.R
+import com.sopt.now.gs.core.base.BindingFragment
+import com.sopt.now.gs.databinding.FragmentHomeBinding
+
+class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    override fun initView() {
+//        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/sopt/now/gs/feature/pay/PayFragment.kt
@@ -0,0 +1,12 @@
+package com.sopt.now.gs.feature.pay
+
+import com.sopt.now.gs.R
+import com.sopt.now.gs.core.base.BindingFragment
+import com.sopt.now.gs.databinding.FragmentHomeBinding
+import com.sopt.now.gs.databinding.FragmentPayBinding
+
+class PayFragment : BindingFragment<FragmentPayBinding>(R.layout.fragment_pay) {
+    override fun initView() {
+//        TODO("Not yet implemented")
+    }
+}
