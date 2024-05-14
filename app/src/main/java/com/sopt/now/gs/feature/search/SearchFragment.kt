f8b67e9bccf4ec13e671e9c306832b6ff310e7ea
chanu <cw8881@naver.com>
2024-05-14 23:00:37 +0900

    #10 [rename] rename

--- app/src/main/java/com/sopt/now/gs/feature/search/SearchFragment.kt
+++ app/src/main/java/com/sopt/now/gs/feature/search/SearchFragment.kt
@@ -2,10 +2,9 @@ package com.sopt.now.gs.feature.search
 
 import com.sopt.now.gs.R
 import com.sopt.now.gs.core.base.BindingFragment
-import com.sopt.now.gs.databinding.FragmentHomeBinding
 import com.sopt.now.gs.databinding.FragmentSearchBinding
 
-class SerachFragment : BindingFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+class SearchFragment : BindingFragment<FragmentSearchBinding>(R.layout.fragment_search) {
     override fun initView() {
 //        TODO("Not yet implemented")
     }
