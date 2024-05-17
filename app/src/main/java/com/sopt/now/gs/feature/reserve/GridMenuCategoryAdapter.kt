89e067eda09172028836d6952413a9aa955c02bb
hyeumm <20221159@sungshin.ac.kr>
2024-05-17 20:20:53 +0900

    #12 [UI] : 메뉴 카테고리

---
+++ app/src/main/java/com/sopt/now/gs/feature/reserve/GridMenuCategoryAdapter.kt
@@ -0,0 +1,51 @@
+package com.sopt.now.gs.feature.reserve
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.sopt.now.gs.R
+
+class GridMenuCategoryAdapter(val context: Context, private val items: MutableList<GridMenuCategoryItem>) :
+    BaseAdapter() {
+    override fun getView(position: Int, p1: View?, p2: ViewGroup?): View {
+        val view : View = LayoutInflater.from(context).inflate(R.layout.item_reserve_menu_category,null)
+
+        val currentItem = items[position]
+
+        view.findViewById<ImageView>(R.id.iv_reserve_menu_category_image).setImageResource(currentItem.menuCategoryImage)
+        view.findViewById<TextView>(R.id.iv_reserve_menu_category_title).text = currentItem.menuCategoryTitle
+
+        return view
+    }
+    override fun getCount(): Int = items.size
+
+    override fun getItem(position: Int): GridMenuCategoryItem = items[position]
+
+    override fun getItemId(position: Int): Long = position.toLong()
+
+//    override fun getView(position: Int, view: View?, parent: ViewGroup?): View {
+//        var convertView = view
+//        val binding: ItemReserveMenuCategoryBinding
+//
+//        if (convertView == null) {
+//            val inflater = LayoutInflater.from(parent?.context)
+//            binding = ItemReserveMenuCategoryBinding.inflate(inflater, parent, false)
+//            convertView = binding.root
+//            convertView.tag = binding
+//        } else {
+//            binding = convertView.tag as ItemReserveMenuCategoryBinding
+//        }
+//
+//        val item: GridMenuCategoryItem = items[position]
+//        binding.ivReserveMenuCategoryImage.setImageResource(item.menuCategoryImage)
+//        binding.ivReserveMenuCategoryTitle.text = item.menuCategoryTitle
+//
+//        return convertView
+//    }
+}
+
+data class GridMenuCategoryItem(val menuCategoryImage: Int, val menuCategoryTitle: String)
--- app/src/main/java/com/sopt/now/gs/feature/reserve/ReserveFragment.kt
+++ app/src/main/java/com/sopt/now/gs/feature/reserve/ReserveFragment.kt
@@ -5,7 +5,22 @@ import com.sopt.now.gs.core.base.BindingFragment
 import com.sopt.now.gs.databinding.FragmentReserveBinding
 
 class ReserveFragment : BindingFragment<FragmentReserveBinding>(R.layout.fragment_reserve) {
+    private val items = mutableListOf<GridMenuCategoryItem>()
     override fun initView() {
-        //TODO
+        //scope함수 처리 필요
+        items.add(GridMenuCategoryItem(R.drawable.img_reserve_pystor,"편스토랑"))
+        items.add(GridMenuCategoryItem(R.drawable.img_reserve_meal,"밀박스25"))
+        items.add(GridMenuCategoryItem(R.drawable.img_reserve_dosyrak,"도시락"))
+        items.add(GridMenuCategoryItem(R.drawable.img_reserve_bread,"빵"))
+        items.add(GridMenuCategoryItem(R.drawable.img_reserve_ganpn,"냉장간편식"))
+        items.add(GridMenuCategoryItem(R.drawable.img_reserve_salad,"샐러드"))
+        items.add(GridMenuCategoryItem(R.drawable.img_reserve_drink,"주류예약"))
+        items.add(GridMenuCategoryItem(R.drawable.img_reserve_salad,"신선식품"))
+        items.add(GridMenuCategoryItem(R.drawable.img_reserve_gimbab,"김밥/주먹밥"))
+        items.add(GridMenuCategoryItem(R.drawable.img_reserve_pasta,"요리반찬/조리면"))
+        items.add(GridMenuCategoryItem(R.drawable.img_reserve_hamburger,"샌드위치/햄버거"))
+
+        val menuCategoryAdapter = GridMenuCategoryAdapter(requireContext(),items)
+        binding.gvReserveMenuCategory.adapter = menuCategoryAdapter
     }
 }
