92ada887ea39d8bfd0d58a76a9889f1706ea8685
hyeumm <20221159@sungshin.ac.kr>
2024-05-17 22:06:56 +0900

    #12 [CHORE] : 메뉴 카테고리 > 메뉴 리스트 변수명 수정

--- app/src/main/java/com/sopt/now/gs/feature/reserve/ReserveFragment.kt
+++ app/src/main/java/com/sopt/now/gs/feature/reserve/ReserveFragment.kt
@@ -6,25 +6,25 @@ import com.sopt.now.gs.core.base.BindingFragment
 import com.sopt.now.gs.databinding.FragmentReserveBinding
 
 class ReserveFragment : BindingFragment<FragmentReserveBinding>(R.layout.fragment_reserve) {
-    private val menuCategoryitems = mutableListOf<GridMenuCategoryItem>()
+    private val menuListitems = mutableListOf<GridMenuListItem>()
     private val viewModel by viewModels<DiscountMenuViewModel>()
 
     override fun initView() {
-        menuCategoryitems.apply {
-            add(GridMenuCategoryItem(R.drawable.img_reserve_pystor, "편스토랑"))
-            add(GridMenuCategoryItem(R.drawable.img_reserve_meal, "밀박스25"))
-            add(GridMenuCategoryItem(R.drawable.img_reserve_dosyrak, "도시락"))
-            add(GridMenuCategoryItem(R.drawable.img_reserve_bread, "빵"))
-            add(GridMenuCategoryItem(R.drawable.img_reserve_ganpn, "냉장간편식"))
-            add(GridMenuCategoryItem(R.drawable.img_reserve_salad, "샐러드"))
-            add(GridMenuCategoryItem(R.drawable.img_reserve_drink, "주류예약"))
-            add(GridMenuCategoryItem(R.drawable.img_reserve_salad, "신선식품"))
-            add(GridMenuCategoryItem(R.drawable.img_reserve_gimbab, "김밥/주먹밥"))
-            add(GridMenuCategoryItem(R.drawable.img_reserve_pasta, "요리반찬/조리면"))
-            add(GridMenuCategoryItem(R.drawable.img_reserve_hamburger, "샌드위치/햄버거"))
+        menuListitems.apply {
+            add(GridMenuListItem(R.drawable.img_reserve_pystor, "편스토랑"))
+            add(GridMenuListItem(R.drawable.img_reserve_meal, "밀박스25"))
+            add(GridMenuListItem(R.drawable.img_reserve_dosyrak, "도시락"))
+            add(GridMenuListItem(R.drawable.img_reserve_bread, "빵"))
+            add(GridMenuListItem(R.drawable.img_reserve_ganpn, "냉장간편식"))
+            add(GridMenuListItem(R.drawable.img_reserve_salad, "샐러드"))
+            add(GridMenuListItem(R.drawable.img_reserve_drink, "주류예약"))
+            add(GridMenuListItem(R.drawable.img_reserve_salad, "신선식품"))
+            add(GridMenuListItem(R.drawable.img_reserve_gimbab, "김밥/주먹밥"))
+            add(GridMenuListItem(R.drawable.img_reserve_pasta, "요리반찬/조리면"))
+            add(GridMenuListItem(R.drawable.img_reserve_hamburger, "샌드위치/햄버거"))
         }
 
-        val menuCategoryAdapter = GridMenuCategoryAdapter(requireContext(), menuCategoryitems)
+        val menuCategoryAdapter = GridMenuCategoryAdapter(requireContext(), menuListitems)
         binding.gvReserveMenuList.adapter = menuCategoryAdapter
 
 
