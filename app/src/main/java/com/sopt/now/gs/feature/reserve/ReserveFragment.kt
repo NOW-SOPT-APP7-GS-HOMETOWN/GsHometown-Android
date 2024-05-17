e480e061d8053880b30b62b7f1c5b198cf88edcf
hyeumm <20221159@sungshin.ac.kr>
2024-05-17 20:23:15 +0900

    #12 [MOD] : 메뉴 카테고리 - apply

--- app/src/main/java/com/sopt/now/gs/feature/reserve/ReserveFragment.kt
+++ app/src/main/java/com/sopt/now/gs/feature/reserve/ReserveFragment.kt
@@ -5,22 +5,23 @@ import com.sopt.now.gs.core.base.BindingFragment
 import com.sopt.now.gs.databinding.FragmentReserveBinding
 
 class ReserveFragment : BindingFragment<FragmentReserveBinding>(R.layout.fragment_reserve) {
-    private val items = mutableListOf<GridMenuCategoryItem>()
+    private val menuCategoryitems = mutableListOf<GridMenuCategoryItem>()
     override fun initView() {
-        //scope함수 처리 필요
-        items.add(GridMenuCategoryItem(R.drawable.img_reserve_pystor,"편스토랑"))
-        items.add(GridMenuCategoryItem(R.drawable.img_reserve_meal,"밀박스25"))
-        items.add(GridMenuCategoryItem(R.drawable.img_reserve_dosyrak,"도시락"))
-        items.add(GridMenuCategoryItem(R.drawable.img_reserve_bread,"빵"))
-        items.add(GridMenuCategoryItem(R.drawable.img_reserve_ganpn,"냉장간편식"))
-        items.add(GridMenuCategoryItem(R.drawable.img_reserve_salad,"샐러드"))
-        items.add(GridMenuCategoryItem(R.drawable.img_reserve_drink,"주류예약"))
-        items.add(GridMenuCategoryItem(R.drawable.img_reserve_salad,"신선식품"))
-        items.add(GridMenuCategoryItem(R.drawable.img_reserve_gimbab,"김밥/주먹밥"))
-        items.add(GridMenuCategoryItem(R.drawable.img_reserve_pasta,"요리반찬/조리면"))
-        items.add(GridMenuCategoryItem(R.drawable.img_reserve_hamburger,"샌드위치/햄버거"))
+        menuCategoryitems.apply {
+            add(GridMenuCategoryItem(R.drawable.img_reserve_pystor,"편스토랑"))
+            add(GridMenuCategoryItem(R.drawable.img_reserve_meal,"밀박스25"))
+            add(GridMenuCategoryItem(R.drawable.img_reserve_dosyrak,"도시락"))
+            add(GridMenuCategoryItem(R.drawable.img_reserve_bread,"빵"))
+            add(GridMenuCategoryItem(R.drawable.img_reserve_ganpn,"냉장간편식"))
+            add(GridMenuCategoryItem(R.drawable.img_reserve_salad,"샐러드"))
+            add(GridMenuCategoryItem(R.drawable.img_reserve_drink,"주류예약"))
+            add(GridMenuCategoryItem(R.drawable.img_reserve_salad,"신선식품"))
+            add(GridMenuCategoryItem(R.drawable.img_reserve_gimbab,"김밥/주먹밥"))
+            add(GridMenuCategoryItem(R.drawable.img_reserve_pasta,"요리반찬/조리면"))
+            add(GridMenuCategoryItem(R.drawable.img_reserve_hamburger,"샌드위치/햄버거"))
+        }
 
-        val menuCategoryAdapter = GridMenuCategoryAdapter(requireContext(),items)
+        val menuCategoryAdapter = GridMenuCategoryAdapter(requireContext(),menuCategoryitems)
         binding.gvReserveMenuCategory.adapter = menuCategoryAdapter
     }
 }
