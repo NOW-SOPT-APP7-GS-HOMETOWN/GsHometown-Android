1c0aa678fb72807728fabb08d0784ded2009eed2
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-05-22 05:15:01 +0900

    #19 [feat] 서버 통신 세팅

--- app/src/main/java/com/sopt/now/gs/data/api/ApiFactory.kt
+++ app/src/main/java/com/sopt/now/gs/data/api/ApiFactory.kt
@@ -2,7 +2,8 @@ package com.sopt.now.gs.data.api
 
 import android.util.Log
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import com.sopt.now.gs.data.api.service.ReqresService
+import com.sopt.now.gs.BuildConfig
+import com.sopt.now.gs.data.api.service.GsHometownService
 import kotlinx.serialization.json.Json
 import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
@@ -11,7 +12,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 
 object ApiFactory {
-    private const val USER_BASE_URL = "https://reqres.in/"
+    private const val USER_BASE_URL = BuildConfig.GSHOMETOWN_BASE_URL
 
     private fun getLogOkHttpClient(): Interceptor {
         val loggingInterceptor = HttpLoggingInterceptor { message ->
@@ -38,8 +39,8 @@ object ApiFactory {
     }
 
     object ServicePool {
-        val userService: ReqresService by lazy {
-            create<ReqresService>(USER_BASE_URL)
+        val userService: GsHometownService by lazy {
+            create<GsHometownService>(USER_BASE_URL)
         }
     }
 }
