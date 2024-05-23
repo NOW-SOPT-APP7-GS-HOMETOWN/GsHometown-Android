96ef4d443bb2c839e9abec1af864aaeb840fbbb5
chanu <cw8881@naver.com>
2024-05-23 17:03:09 +0900

    #29 [feat] : 상품 상세 보기 화면 서버 통신 구현

--- app/src/main/java/com/sopt/now/gs/feature/purchasedetail/PurchaseDetailFragment.kt
+++ app/src/main/java/com/sopt/now/gs/feature/purchasedetail/PurchaseDetailFragment.kt
@@ -1,14 +1,42 @@
 package com.sopt.now.gs.feature.purchasedetail
 
+import androidx.fragment.app.viewModels
+import coil.load
 import com.sopt.now.gs.R
 import com.sopt.now.gs.core.base.BindingFragment
+import com.sopt.now.gs.core.view.UiState
+import com.sopt.now.gs.data.response.ResponsePurchaseDetailDto
 import com.sopt.now.gs.databinding.FragmentPurchaseDetailBinding
+import com.sopt.now.gs.feature.util.PriceFormatter
 
 class PurchaseDetailFragment :
     BindingFragment<FragmentPurchaseDetailBinding>(R.layout.fragment_purchase_detail) {
+    private val viewModel by viewModels<PurchaseDetailViewModel>()
     override fun initView() {
         initHeartBtnClickListener()
         initFabBtnClickListener()
+        initGetPurchaseDetail()
+    }
+
+    private fun initGetPurchaseDetail() {
+        viewModel.getPurchaseDetail(1.toLong())
+        viewModel.purchaseDetailState.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is UiState.Success -> {
+                    handlePurchaseDetailUi(state.data)
+                }
+
+                else -> Unit
+            }
+        }
+    }
+
+    private fun handlePurchaseDetailUi(data: ResponsePurchaseDetailDto) = with(binding) {
+        ivPurchaseDetailTop.load(data.thumbnail)
+        tvPurchaseDetailTitle.text = data.title
+        tvPurchaseDetailPrice.text = PriceFormatter.formatPrice(data.price)
+        tvPurchaseDetailReviewCount.text = "(${data.reviewCount})"
+        ivPurchaseDetailMain.load(data.detailImage)
     }
 
     private fun initHeartBtnClickListener() {
